b8d3f61376533a324e157446103f25222f78cb33
Isadora Ferreira Alvarenga <1558374@sga.pucminas.br>
2026-09-23 11:17:59 -0300

    Adiciona PROVA1DIAW ao repositório

---
+++ Prova1DIAW/src/main/java/com/example/Prova1DIAW/Prova1DiawApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prova1DIAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1DiawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1DiawApplication.class, args);
+	}
+
+}
---
+++ Prova1DIAW/src/test/java/com/example/Prova1DIAW/Prova1DiawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prova1DIAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova1DiawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
